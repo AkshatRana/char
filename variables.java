be179e02d8177191e5da14d3b50432ea0d08d892
AkshatRana <akshat.xworkz@gmail.com>
2021-12-20 18:10:21 +0530

    Added

---
+++ variables.java
@@ -0,0 +1,26 @@
+class variables{
+	public static void main(String[] args){
+	// datatype rvn = value;
+		byte ab= 10;
+		short ss= 20;
+		int ii= 22;
+		long lg= 10101010;
+
+		float ff= 10.12f;
+		double dd= 2.56;
+
+		boolean bb= true;
+
+		char ch= 'A';
+			System.out.println(ab);
+			System.out.println(ss);
+			System.out.println(ii);
+			System.out.println(lg);
+			System.out.println(ff);
+			System.out.println(dd);
+			System.out.println(bb);
+			System.out.println(ch);
+}
+
+
+}
